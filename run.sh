38a79cb293e009e3c7d1bec04bb1847eed57adee
kiko.devv <kiko.devv@gmail.com>
2025-08-24 20:55:59 +0200

    added som fun for menu ui

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-clear
-javac src/Main.java && java -cp src Main
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,50 @@
+import java.util.Scanner;
 public class Main {
     //-----------functions-----------//
-    public static void uiHome() {
+    public static void uiHome( String userName) {
         for (int i = 0; i < 9; i++) {
             System.out.println("                                   :");
         }
-        System.out.println("I-----------------------[ Welcome to EasyBudget ]---------------------I");
-        for (int i = 0; i < 21; i++) {
-            System.out.println("|                                                                     |");
-        }
+        System.out.println("I-------------------[ Welcome to EasyBudget " + userName + " ]---------------------I");
+        System.out.println("I                                                                     I");
+        System.out.println("I    Add                                      I");
+        System.out.println("I-------------------[ 3. Edit Budget ]--------------------------------I");
+        System.out.println("I-------------------[ 4. Delete Budget ]------------------------------I");
+        System.out.println("I-------------------[ 5. Exit ]---------------------------------------I");
         System.out.println("I---------------------------------------------------------------------I");
-        for (int i = 0; i < 10; i++) {
+    }
+    public static void nextPage( int pageNumber) {
+        for (int i = 0; i <pageNumber; i++) {
             System.out.println("                                   :");
         }
     }
 
+    public static void marginBottom (int mb ) {
+        for (int i = 0; i < mb; i++) {
+            System.out.println(" ");
+        }
+    }
 
+
+    //-----------Root-----------//
     public static void main(String[] args) {
-        uiHome();
+        Scanner scanner = new Scanner(System.in);
+        //---User input name---//
+        nextPage(10);
+        System.out.println("I-----------------------[ Welcome to EasyBudget ]---------------------I");
+        System.out.print("Please enter your name here-->");
+        String userName = scanner.nextLine();
+        nextPage(100);
+        System.out.println("I-----------------------[ Welcome to EasyBudget " + userName + " ]---------------------I");
+        System.out.print("Please enter your earning");
+        marginBottom(20);
+        int income = scanner.nextInt();
+
+
+
+
+
+
+        scanner.close();
     }
 }
